43b49c3e4bffc434db3b01466a4d5f536b36a8e2
AssahiAbdelhak <bdalhqa89@gmail.com>
2026-01-31 16:53:31 +0100

    fix : terraform config

--- terraform/security/main.tf
+++ terraform/security/main.tf
@@ -22,3 +22,9 @@ resource "aws_vpc_security_group_ingress_rule" "allow_http_port" {
   ip_protocol       = "tcp"
   to_port           = 80
 }
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_egress" {
+  security_group_id = aws_security_group.sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
--- terraform/variables.auto.tfvars
+++ terraform/variables.auto.tfvars
@@ -1,2 +1,2 @@
-ami = "ami-00634bca710e8ccb1"
+ami = "ami-04df1508c6be5879e"
 instance_type = "t3.micro"
