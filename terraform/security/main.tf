154fb0c8cf22fc589d6c1f24e5028b01eb4539af
AssahiAbdelhak <bdalhqa89@gmail.com>
2026-01-31 15:54:21 +0100

    add : terraform config

---
+++ terraform/security/main.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "sg" {
+  name        = "Security group"
+  description = "This is my security group"
+  vpc_id      = var.vpc_id
+  tags = {
+    Name = "SG"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_port" {
+  security_group_id = aws_security_group.sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http_port" {
+  security_group_id = aws_security_group.sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
---
+++ terraform/variables.auto.tfvars
@@ -0,0 +1,2 @@
+ami = "ami-00634bca710e8ccb1"
+instance_type = "t3.micro"
